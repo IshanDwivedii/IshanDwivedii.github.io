3baa7079a1715bda84e9bc9964378a502eaf53b5
Ishan Dwivedi <Idwivedi@Securonixs-MacBook-Pro.local>
2025-02-19 20:41:25 +0530

    Remove all files

--- data/headerNavLinks.js
+++
@@ -1,8 +0,0 @@
-const headerNavLinks = [
-  { href: '/about', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/tags', title: 'Tags' },
-]
-
-export default headerNavLinks
